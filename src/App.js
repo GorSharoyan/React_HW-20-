2c41097f6bd03129f13781c85037e5839382e2d2
Gor <gor.sharoyan95@gmail.com>
2020-09-08 01:57:00 +0400

    created my own App.js/completed all necessary import/exports ,created HTML structure

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return(
+            <div>
+                <div>
+                    <button>increase</button>
+                    <button>decrease</button>
+                    <button>reset</button>
+                </div>
+                <div>
+                    <input placeholder={'minimum value'}/>
+                    <input placeholder={'maximum value'}/>
+                    <input placeholder={'step'}/>
+                </div>
+             </div>   
+        )
+    }
 }
-
-export default App;
