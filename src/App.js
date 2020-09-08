6c5190fa08d8274bb29413214805056611493800
Gor <gor.sharoyan95@gmail.com>
2020-09-09 01:11:11 +0400

    added minimal css value to the main page /also added some headers

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './App.css'
 
 export default class App extends React.Component{
     constructor(props){
@@ -54,30 +55,41 @@ export default class App extends React.Component{
     }
     handleSubmit=(event)=>{
         event.preventDefault();
-        
+        // thie.setState({
+            
+        // })
     }
 
 
 //Render
     render(){
         return(
-            <div>
-                <div>
+            <div className='counter-app'>
+                <div className='button-bar'>
+                    <h1>Counter App</h1>
+                </div>
+                <div className='button-bar'>
                     <button onClick={this.handleIncrease}>increase</button>
                     <button onClick={this.handleDecrease}>decrease</button>
                     <button onclick={this.handleReset}>reset</button>
                 </div>
-                <div>
+                <div className='input-form'>
                     <form onSubmit={this.handleSubmit}> 
-                        <label>
+                        <label >
+                            <h3>Set Minimum Value</h3>
                             <input value={this.state.minVal} onChange={this.handleMinVal} placeholder={'minimum value'}/>
                         </label>
+                        <br></br>
                         <label>
+                            <h3>Set Maximum Value</h3>
                             <input value={this.state.maxVal} onChange={this.handleMaxVal} placeholder={'maximum value'}/>
                         </label>
+                        <br></br>
                         <label>
+                            <h3>Set Step</h3>
                             <input value={this.state.step}   onChange={this.handleStep} placeholder={'step'}/>
                         </label>
+                        <br></br>
                         <button>Subbmit</button>
                     </form>
                 </div>
